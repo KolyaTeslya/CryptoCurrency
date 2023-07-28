196cf77e369fc8a3c6299c5525f775409b8e654d
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-07-28 10:40:49 +0300

    crypto init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(10),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  cryptoInputBox: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className={classes.root} maxWidth="lg">
+      <Grid container spacing={3}>
+        <Grid item xs={8}>
+          <Paper className={classes.paper}></Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>
+            <div className={classes.cryptoInputBox}>
+              <TextField fullWidth label="Валюта"  />
+              <Select
+                value={10} >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </div>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
